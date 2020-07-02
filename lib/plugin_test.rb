f5eedc736e1cfec8b4919004e0c91d591bee7804
Tyler Ewing <tewing10@gmail.com>
2020-07-02 14:57:42 -0500

    Try bootboot pattern

--- lib/plugin_test.rb
+++ lib/plugin_test.rb
@@ -5,10 +5,14 @@ module PluginTest
   class Error < StandardError; end
 
   class Command < Bundler::Plugin::API
-    command "plugin_test"
+    def setup
+      self.class.command "plugin_test"
+    end
 
     def exec(command_name, args)
       puts "You called " + command_name + " with args: " + args.inspect
     end
   end
 end
+
+PluginTest::Command.new.setup
