c22be547d91cf15144162ba277ac1bf415d5279c
Tyler Ewing <tewing10@gmail.com>
2020-07-04 18:44:33 -0500

    debug info

--- lib/plugin_test/hook.rb
+++ lib/plugin_test/hook.rb
@@ -10,20 +10,23 @@ module PluginTest
       self.class.hook("after-install-all") do
         current_definition = Bundler.definition
         unlock = current_definition.instance_variable_get(:@unlock)
+        next_definition = Bundler::Definition.build(
+          Pathname(File.expand_path("Gemfile")),
+          Pathname(File.expand_path("Gemfile_next.lock")),
+          unlock
+        )
+        Bundler.ui.confirm("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        Bundler.ui.confirm(unlock.inspect)
+        Bundler.ui.confirm("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+        if current_definition.to_lock != @previous_lockfile
+          next_definition.resolve_remotely!
+        end
+
+        Bundler.ui.confirm("\nNow bundling for NEXT\n")
 
         begin
           ENV["DEPENDENCY_NEXT_OVERRIDE"] = "1"
-          next_definition = Bundler::Definition.build(
-            Pathname(File.expand_path("Gemfile")),
-            Pathname(File.expand_path("Gemfile_next.lock")),
-            unlock
-          )
-
-          if current_definition.to_lock != @previous_lockfile
-            next_definition.resolve_remotely!
-          end
-
-          Bundler.ui.confirm("\nNow bundling for NEXT\n")
           Bundler::Installer.new(Bundler.root, next_definition).run({})
         ensure
           ENV.delete("DEPENDENCY_NEXT_OVERRIDE")
