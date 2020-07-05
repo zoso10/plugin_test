704c70259d8455125d280f33251c823e39f4c8af
Tyler Ewing <tewing10@gmail.com>
2020-07-05 10:03:44 -0500

    fix init command

--- lib/plugin_test/command.rb
+++ lib/plugin_test/command.rb
@@ -8,18 +8,13 @@ module PluginTest
 
     def exec(command, args)
       if args.include?("--init")
-        lockfile = File.expand_path("Gemfile.lock")
-        next_lockfile = File.expand_path("Gemfile_next.lock")
+        lockfile_path = File.expand_path("Gemfile.lock")
+        next_lockfile_path = File.expand_path("Gemfile_next.lock")
 
         if File.exists?(next_lockfile)
           Bundler.ui.warn("Gemfile_next.lock already exists. Skipping.")
         else
-          begin
-            ENV["DEPENDENCY_NEXT_OVERRIDE"] = "1"
-            FileUtils.cp(lockfile_path, next_lockfile_path)
-          ensure
-            ENV.delete("DEPENDENCY_NEXT_OVERRIDE")
-          end
+          FileUtils.cp(lockfile_path, next_lockfile_path)
         end
       else
         puts "You called " + command_name + " with args: " + args.inspect
